cc701795a55b8e7da9b8f374ca92f7cf29f302c2
mookamir021021021-blip <mookamir021021021@gmail.com>
2026-08-12 11:48:21 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,1 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID"))
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
+dg
